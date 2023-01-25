8058ae0cfecb3e6030189abffb9d8b3b16b6ea77
mateusprogaming <mateus_silvah@hotmail.com>
2023-01-25 03:50:26 -0300

    first commit

--- src/pages/programming/index.tsx
+++ src/pages/programming/index.tsx
@@ -1,14 +1,15 @@
 import CabecalhoProgramming from "../../components/Programming/Cabecalho/CabecalhoProgramming";
 import CorpoProgramming from "../../components/Programming/Corpo/CorpoProgramming";
 import SubCabecalhoProgramming from "../../components/Programming/SubCabecalhoProgramming/SubCabecalhoProgramming";
+import styles from "../../styles/programming.module.css"
 
 
 export default function Programming() {
     return (
-        <>
+        <div className={styles.container}>
             <CabecalhoProgramming />
             <SubCabecalhoProgramming />
             <CorpoProgramming />
-        </>
+        </div>
     )
 }
