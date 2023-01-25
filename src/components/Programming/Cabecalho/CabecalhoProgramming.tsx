498af9b96049ca45ab7ab6a75e5d72e98eda561c
mateusprogaming <mateus_silvah@hotmail.com>
2023-01-25 03:03:43 -0300

    first commit

--- src/components/Programming/Cabecalho/CabecalhoProgramming.tsx
+++ src/components/Programming/Cabecalho/CabecalhoProgramming.tsx
@@ -3,7 +3,7 @@ import styles from '../../../styles/cabecalhoProgramming.module.css'
 export default function CabecalhoProgramming() {
     return (
         <div className={styles.ContainerGlobal}>
-            <h1>Programming Skills</h1>
+            <h1 className={styles.Titulo}>Programming Skills</h1>
         </div>
     )
 }
--- src/components/Programming/SubCabecalhoProgramming/SubCabecalhoProgramming.tsx
+++ src/components/Programming/SubCabecalhoProgramming/SubCabecalhoProgramming.tsx
@@ -6,13 +6,13 @@ export default function SubCabecalhoProgramming() {
     return (
         <div className={styles.ContainerGlobal}>
             <Link href={'/'} className={styles.link}>
-                <h1 className={styles.cursor}><FaFastBackward /></h1>
+                <h1><FaFastBackward /></h1>
             </Link>
             <Link href={'/'} className={styles.link}>
-                <h1 className={styles.cursor}><FaHome /></h1>
+                <h1><FaHome /></h1>
             </Link>
             <Link href={'/piscicology'} className={styles.link}>
-                <h1 className={styles.cursor}><FaFastForward /></h1>
+                <h1><FaFastForward /></h1>
             </Link>
         </div>
     )
