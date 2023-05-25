6f49caf5c1834190d20d1c5c4c2db961bd3d40e2
mateusprogaming <mateus_silvah@hotmail.com>
2023-05-25 10:04:25 -0300

    Atualizaca 2.8

--- src/pages/Engenharia/index.tsx
+++ src/pages/Engenharia/index.tsx
@@ -1,6 +1,6 @@
 import CorpoEngenharia from "@/src/components/Engenharia/CorpoEngenharia";
 import CabecalhoEngenharia from "../../components/Engenharia/CabecalhoEngenharia";
-import styles from "../../styles/Engenharia.module.css"
+import styles from "../../styles/engenharia.module.css"
 export default function Engenharia() {
     return (
         <div className={styles.containerGlobal}>
